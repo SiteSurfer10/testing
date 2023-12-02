52f14173303ec4b913ae04f74bbdab53613a3349
SiteSurfer10 <114467127+SiteSurfer10@users.noreply.github.com>
2023-12-02 10:24:23 +0100

    Add files via upload

---
+++ testing.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Mijn eerste webpagina</title>
+<meta name="description" content="Mijn eerste webpagina">
+</head>
+<body>
+    <h1>I am HTML markup.</h1>
+<h1>I am HTML markup.</h1>
+<?php echo '<p>And I am PHP.</p>'; ?>
+</body>
+</html>
